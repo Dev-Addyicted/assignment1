95b168dea41edd202b3d3bedcbed7a52b8daf91c
Dev-Addyicted <devadnanreactjs@gmail.com>
2023-10-28 17:00:28 +0530

    Added Last update

---
+++ src/components/Privacypolicy.jsx
@@ -0,0 +1,16 @@
+import { Grid } from '@mui/material';
+import React from 'react';
+
+const Privacypolicy = () => {
+    return (
+        <Grid container spacing={2}>
+            <Grid item xs={1}></Grid>
+            <Grid item xs={10}>
+
+            </Grid>
+            <Grid item xs={1}></Grid>
+        </Grid>
+    );
+};
+
+export default Privacypolicy;
